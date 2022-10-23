ed0af8f69ebb7195ddbd1d39bb63ee78cd107989
DevelSajjad <sinigdho01@gmail.com>
2022-10-23 22:01:43 +0600

    User Sidebar Mastering

---
+++ resources/views/user/include/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="col-sm-6">
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+            <img class="card-img-top" style="border-radius: 50%" src="{{ asset(Auth::user()->image) }}"
+                alt="Card image cap" height="40%" width="100%" alt="Profile Image">
+
+        </div>
+        <ul class="list-group list-group-flush">
+            <a href="{{ route('user.dashboard') }}" class="btn btn-primary btn-sm btn-block">Home</a>
+            <a href="{{ route('user-image') }}" class="btn btn-primary btn-sm btn-block">Update Image</a>
+            <a href="{{ route('update-password') }}" class="btn btn-primary btn-sm btn-block">Change Password</a>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="btn btn-danger btn-sm btn-block">Logout</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                @csrf
+            </form>
+        </ul>
+    </div>
+</div>
--- resources/views/user/userimage.blade.php
+++ resources/views/user/userimage.blade.php
@@ -17,21 +17,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-header">
-                            <img class="card-img-top" style="border-radius: 50%" src="{{ asset(Auth::user()->image) }}"
-                                alt="Card image cap" height="40%" width="100%" alt="Profile Image">
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            <a href="#" class="btn btn-primary btn-sm btn-block">Edit Profile</a>
-                            <a href="#" class="btn btn-primary btn-sm btn-block">Update Image</a>
-                            <a href="{{ route('update-password') }}" class="btn btn-primary btn-sm btn-block">Change Password</a>
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="btn btn-danger btn-sm btn-block">Logout</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                @csrf
-                            </form>
-                        </ul>
-                    </div>
+                   @include('user.include.sidebar')
                 </div>
                 <div class="col-sm-6">
                     <div class="card">
